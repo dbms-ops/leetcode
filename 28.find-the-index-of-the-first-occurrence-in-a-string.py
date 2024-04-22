675c5ed0593d0c2e36dd5984b391c1af479b1dcb
lixun1 <lixun1@lixiang.com>
2024-04-22 22:23:26 +0800

    利用 enumerate 获取子串第一次出现的 index

---
+++ 28.find-the-index-of-the-first-occurrence-in-a-string.py
@@ -0,0 +1,40 @@
+# @lcpr-before-debug-begin
+from operator import le
+from python3problem28 import *
+from typing import *
+
+# @lcpr-before-debug-end
+
+#
+# @lc app=leetcode.cn id=28 lang=python3
+# @lcpr version=30122
+#
+# [28] 找出字符串中第一个匹配项的下标
+#
+
+
+# @lcpr-template-start
+
+
+# @lcpr-template-end
+# @lc code=start
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        for index, value in enumerate(haystack):
+            if haystack[index:index + len(needle)] == needle:
+                return index
+        return -1
+
+# @lc code=end
+
+
+#
+# @lcpr case=start
+# "sadbutsad"\n"sad"\n
+# @lcpr case=end
+
+# @lcpr case=start
+# "leetcode"\n"leeto"\n
+# @lcpr case=end
+
+#
